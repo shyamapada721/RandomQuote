38ab18b4f88b43a3c118daa736cdc7a4210bbccb
Shyamapada Chowdhury <70567947+shyamapada721@users.noreply.github.com>
2023-08-30 21:25:57 +0530

    [Backend] Removed the use of API for Quotes

---
+++ quotes.js
@@ -0,0 +1,446 @@
+
+export const quoteArr = [
+  {
+    "text": "Genius is one percent inspiration and ninety-nine percent perspiration.",
+    "author": "Thomas Edison, type.fit"
+  },
+  {
+    "text": "You can observe a lot just by watching.",
+    "author": "Yogi Berra, type.fit"
+  },
+  {
+    "text": "A house divided against itself cannot stand.",
+    "author": "Abraham Lincoln, type.fit"
+  },
+  {
+    "text": "Difficulties increase the nearer we get to the goal.",
+    "author": "Johann Wolfgang von Goethe, type.fit"
+  },
+  {
+    "text": "Fate is in your hands and no one elses",
+    "author": "Byron Pulsifer, type.fit"
+  },
+  {
+    "text": "Be the chief but never the lord.",
+    "author": "Lao Tzu, type.fit"
+  },
+  {
+    "text": "Nothing happens unless first we dream.",
+    "author": "Carl Sandburg, type.fit"
+  },
+  {
+    "text": "Well begun is half done.",
+    "author": "Aristotle, type.fit"
+  },
+  {
+    "text": "Life is a learning experience, only if you learn.",
+    "author": "Yogi Berra"
+  },
+  {
+    "text": "Self-complacency is fatal to progress.",
+    "author": "Margaret Sangster, type.fit"
+  },
+  {
+    "text": "Peace comes from within. Do not seek it without.",
+    "author": "Buddha, type.fit"
+  },
+  {
+    "text": "What you give is what you get.",
+    "author": "Byron Pulsifer, type.fit"
+  },
+  {
+    "text": "We can only learn to love by loving.",
+    "author": "Iris Murdoch, type.fit"
+  },
+  {
+    "text": "Life is change. Growth is optional. Choose wisely.",
+    "author": "Karen Clark, type.fit"
+  },
+  {
+    "text": "You'll see it when you believe it.",
+    "author": "Wayne Dyer, type.fit"
+  },
+  {
+    "text": "Today is the tomorrow we worried about yesterday.",
+    "author": "type.fit"
+  },
+  {
+    "text": "The mind is everything. What you think, you become.",
+    "author": "Buddha"
+  },
+  {
+    "text": "Happiness is not something ready made. It comes from your own actions.",
+    "author": "Buddha"
+  },
+  {
+    "text": "The only way to do great work is to love what you do. If you haven't found it yet, keep looking. Don't settle.",
+    "author": "Steve Jobs"
+  },
+  {
+    "text": "The journey of a thousand miles begins with a single step.",
+    "author": "Lao Tzu"
+  },
+  {
+    "text": "The best way to predict the future is to create it.",
+    "author": "Abraham Lincoln"
+  },
+  {
+    "text": "The only thing that is constant is change.",
+    "author": "Heraclitus"
+  },
+  {
+    "text": "The greatest glory in living lies not in never falling, but in rising every time we fall.",
+    "author": "Nelson Mandela"
+  },
+  {
+    "text": "The only way to find true happiness is to risk being completely vulnerable.",
+    "author": "C.G. Jung"
+  },
+  {
+    "text": "The greatest gift you can give someone is your time.",
+    "author": "Jim Rohn"
+  },
+  {
+    "text": "The best and most beautiful things in the world cannot be seen or even touched - they must be felt with the heart.",
+    "author": "Helen Keller"
+  },
+  {
+    "text": "Wherever you go, go with all your heart.",
+    "author": "Confucius"
+  },
+  {
+    "text": "The only thing that stands between you and your dream is the will to try and the belief that it is actually possible.",
+    "author": "Joel Brown"
+  },
+  {
+    "text": "The mind is everything. What you think you become.",
+    "author": "Buddha"
+  },
+  {
+    "text": "Holding on to anger is like drinking poison and expecting the other person to die.",
+    "author": "Buddha"
+  },
+  {
+    "text": "The only way to find true happiness is to let go of the things that make you unhappy.",
+    "author": "Buddha"
+  },
+  {
+    "text": "The greatest enemy is yourself. If you can conquer yourself, then you can conquer anything.",
+    "author": "Buddha"
+  },
+  {
+    "text": "The past is gone, the future is not yet here. All we have is the present moment.",
+    "author": "Buddha"
+  },
+  {
+    "text": "If you are patient, you will eventually win.",
+    "author": "Buddha"
+  },
+  {
+    "text": "The only way to get out of the suffering is to go through it.",
+    "author": "Buddha"
+  },
+  {
+    "text": "The greatest gift is the gift of freedom. The greatest freedom is the freedom of the mind.",
+    "author": "Buddha"
+  },
+  {
+    "text": "The past is gone, the future is not yet here. There's only one moment, and it's called the present.",
+    "author": "Buddha"
+  },
+  {
+    "text": "If you are facing a difficulty, there is no need to be troubled. Simply recognize that this is a challenge to your courage and your wisdom.",
+    "author": "Buddha"
+  },
+  {
+    "text": "Do not dwell in the past, do not dream of the future, concentrate the mind on the present moment.",
+    "author": "Buddha"
+  },
+  {
+    "text": "The only real failure in life is not to be true to the best one knows.",
+    "author": "Buddha"
+  },
+  {
+    "text": "If you find no joy in being with yourself, don't expect to find it elsewhere.",
+    "author": "Buddha"
+  },
+  {
+    "text": "The greatest deception men suffer is from their own opinions.",
+    "author": "Buddha"
+  },
+  {
+    "text": "No one is born hating another person because of the color of his skin, or his background, or his religion. People must learn to hate, and if they can learn to hate, they can be taught to love, for love comes more naturally to the human heart than its opposite.",
+    "author": "Martin Luther King, Jr."
+  },
+  {
+    "author": "Walt Disney",
+    "text": "The way to get started is to quit talking and begin doing."
+  },
+  {
+    "author": "Walt Disney",
+    "text": "All our dreams can come true, if we have the courage to pursue them."
+  },
+  {
+    "author": "Walt Disney",
+    "text": "If you can dream it, you can do it."
+  },
+  {
+    "author": "Walt Disney",
+    "text": "We keep moving forward, opening new doors, and doing new things, because we're curious and curiosity keeps leading us down new paths."
+  },
+  {
+    "author": "Walt Disney",
+    "text": "The greatest glory in living lies not in never falling, but in rising every time we fall."
+  },
+  {
+    "author": "Walt Disney",
+    "text": "Tomorrow belongs to those who believe in the beauty of their dreams."
+  },
+  {
+    "author": "Walt Disney",
+    "text": "The imagination is the only limit to what we can achieve."
+  },
+  {
+    "author": "Walt Disney",
+    "text": "The more you are in a state of gratitude, the more you will attract things to be grateful for."
+  },
+  {
+    "author": "Walt Disney",
+    "text": "Laughter is timeless, imagination has no age, and dreams are forever."
+  },
+  {
+    "author": "Steve Jobs",
+    "text": "The only way to do great work is to love what you do. If you haven't found it yet, keep looking. Don't settle."
+  },
+  {
+    "author": "Steve Jobs",
+    "text": "Your time is limited, so don't waste it living someone else's life. Don't be trapped by the dogma of what you think you should be."
+  },
+  {
+    "author": "Steve Jobs",
+    "text": "Don't let the noise of others' opinions drown out your own inner voice. And most important, have the courage to follow your heart and intuition."
+  },
+  {
+    "author": "Steve Jobs",
+    "text": "Innovation distinguishes between a leader and a follower."
+  },
+  {
+    "author": "Steve Jobs",
+    "text": "Design is not just what it looks like and feels like. Design is how it works."
+  },
+  {
+    "author": "Steve Jobs",
+    "text": "The people who are crazy enough to think they can change the world are the ones who do."
+  },
+  {
+    "author": "Steve Jobs",
+    "text": "Stay hungry, stay foolish."
+  },
+  {
+    "author": "Mother Teresa",
+    "text": "The biggest disease today is not leprosy or TB, but rather the feeling of being unwanted, uncared for, and of being of no use in the world."
+  },
+  {
+    "author": "Mother Teresa",
+    "text": "We ourselves feel that what we are doing is just a drop in the ocean. But the ocean would be less because of that missing drop."
+  },
+  {
+    "author": "Mother Teresa",
+    "text": "If you can't feed a hundred people, feed just one."
+  },
+  {
+    "author": "Mother Teresa",
+    "text": "Love is the only force capable of transforming an enemy into friend."
+  },
+  {
+    "author": "Mother Teresa",
+    "text": "We should not judge people. We should try to understand them. Everyone is fighting a battle we know nothing about."
+  },
+  {
+    "author": "Mother Teresa",
+    "text": "Let us not be satisfied with just giving money. Money can be spent for evil as well as for good. It is not enough to just write a check. We must use our hands, our feet, our hearts to reach out to the poor, to touch them, to love them."
+  },
+  {
+    "author": "Mother Teresa",
+    "text": "The greatest gift of all is the gift of giving."
+  },
+  {
+    "author": "Mother Teresa",
+    "text": "The hunger for love is much more difficult to remove than the hunger for bread."
+  },
+  {
+    "author": "Mother Teresa",
+    "text": "We can do no great things, only small things with great love."
+  },
+  {
+    "author": "Mother Teresa",
+    "text": "I do not pray for a lighter burden, but for a stronger back."
+  },
+  {
+    "author": "Mother Teresa",
+    "text": "If you judge people, you have no time to love them."
+  },
+  {
+    "author": "Mother Teresa",
+    "text": "Spread love everywhere you go. Let no one ever come to you without leaving happier."
+  },
+  {
+    "author": "Mother Teresa",
+    "text": "We can't all do great things, but we can do small things with great love."
+  },
+  {
+    "author": "Mother Teresa",
+    "text": "Be kind to everyone you meet, even to those who are unkind to you."
+  },
+  {
+    "author": "Mother Teresa",
+    "text": "We must find the right balance between work and prayer. If we neglect our work, we cannot help the poor. If we neglect our prayer, we cannot find the strength to work."
+  },
+  {
+    "author": "Mother Teresa",
+    "text": "God doesn't ask us to do great things. He asks us to do small things with great love."
+  },
+  {
+    "author": "Mother Teresa",
+    "text": "The simplest things are often the most profound."
+  },
+  {
+    "author": "Mother Teresa",
+    "text": "We are all God's children, and we are all equal in His eyes."
+  },
+  {
+    "author": "Mahatma Gandhi",
+    "text": "Be the change you want to see in the world."
+  },
+  {
+    "author": "Mahatma Gandhi",
+    "text": "The weak can never forgive. Forgiveness is the attribute of the strong."
+  },
+  {
+    "author": "Mahatma Gandhi",
+    "text": "Happiness is when what you think, what you say, and what you do are in harmony."
+  },
+  {
+    "author": "Mahatma Gandhi",
+    "text": "The greatness of a nation and its moral progress can be judged by the way in which its animals are treated."
+  },
+  {
+    "author": "Mahatma Gandhi",
+    "text": "The difference between what we do and what we are capable of doing would suffice to solve most of the world's problems."
+  },
+  {
+    "author": "Mahatma Gandhi",
+    "text": "If we are to achieve real peace in the world, we must begin with the children."
+  },
+  {
+    "author": "Mahatma Gandhi",
+    "text": "Strength does not come from physical capacity. It comes from an indomitable will."
+  },
+  {
+    "author": "Mahatma Gandhi",
+    "text": "A man is but the product of his thoughts. What he thinks, he becomes."
+  },
+  {
+    "author": "Mahatma Gandhi",
+    "text": "The future depends on what we do in the present."
+  },
+  {
+    "author": "Mahatma Gandhi",
+    "text": "An eye for an eye only ends up making the whole world blind."
+  },
+  {
+    "author": "Mahatma Gandhi",
+    "text": "You must be the change you wish to see in the world."
+  },
+  {
+    "author": "Mahatma Gandhi",
+    "text": "Love is the strongest force the world possesses, and yet it is the force least used."
+  },
+  {
+    "author": "Mahatma Gandhi",
+    "text": "The best way to find yourself is to lose yourself in the service of others."
+  },
+  {
+    "author": "Mahatma Gandhi",
+    "text": "The world is a dangerous place, not because of those who do evil, but because of those who look on and do nothing."
+  },
+  {
+    "author": "Mahatma Gandhi",
+    "text": "If we could change ourselves, the world would change. As a man changes his own nature, so does the attitude of the world change towards him."
+  },
+  {
+    "author": "Albert Einstein",
+    "text": "Imagination is more important than knowledge. For knowledge is limited, whereas imagination embraces the entire world, stimulating progress, giving birth to evolution."
+  },
+  {
+    "author": "Albert Einstein",
+    "text": "The only real valuable thing is intuition."
+  },
+  {
+    "author": "Albert Einstein",
+    "text": "The only thing that interferes with my learning is my education."
+  },
+  {
+    "author": "Albert Einstein",
+    "text": "The important thing is not to stop questioning. Curiosity has its own reason for existing."
+  },
+  {
+    "author": "Albert Einstein",
+    "text": "The world as we have created it is a process of our thinking. It cannot be changed without changing our thinking."
+  },
+  {
+    "author": "Albert Einstein",
+    "text": "Weakness of attitude becomes weakness of character."
+  },
+  {
+    "author": "Albert Einstein",
+    "text": "The only way to do great work is to love what you do."
+  },
+  {
+    "author": "Albert Einstein",
+    "text": "The only thing that will redeem mankind is cooperation."
+  },
+  {
+    "author": "Albert Einstein",
+    "text": "The only thing that is constant is change."
+  },
+  {
+    "author": "Albert Einstein",
+    "text": "I never think of the future. It comes soon enough."
+  },
+  {
+    "author": "Albert Einstein",
+    "text": "Logic will get you from A to B. Imagination will take you everywhere."
+  },
+  {
+    "author": "Albert Einstein",
+    "text": "I have no special talents. I am only passionately curious."
+  },
+  {
+    "author": "Albert Einstein",
+    "text": "The only reason for time is so that everything doesn't happen at once."
+  },
+  {
+    "author": "Albert Einstein",
+    "text": "Reality is merely an illusion, albeit a very persistent one."
+  },
+  {
+    "author": "Albert Einstein",
+    "text": "The difference between stupidity and genius is that genius has its limits."
+  },
+  {
+    "author": "Albert Einstein",
+    "text": "The most important thing is to never stop questioning."
+  },
+  {
+    "author": "Albert Einstein",
+    "text": "The only thing that you absolutely have to know, is that you know nothing for certain."
+  },
+  {
+    "author": "Albert Einstein",
+    "text": "The greatest enemy of knowledge is not ignorance, it is the illusion of knowledge."
+  },
+];
+
+
+
